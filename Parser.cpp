7dc0fea275706c08d8154870c4c8554818b8bc25
h1J4cker <33754132+Survive2@users.noreply.github.com>
2022-11-21 16:54:21 +0800

    Update Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -225,16 +225,3 @@ static void HandleTopLevelExpression() {
     getNextToken();
   }
 }
-
-static void MainLoop() {
-  while (1) {
-    fprintf(stderr, "ready> ");
-    switch (CurTok) {
-    case tok_eof:    return;
-    case ';':        getNextToken(); break;  
-    case tok_def:    HandleDefinition(); break;
-    case tok_extern: HandleExtern(); break;
-    default:         HandleTopLevelExpression(); break;
-    }
-  }
-}
